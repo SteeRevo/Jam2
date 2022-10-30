4cfa62b361451ca16c44b9980faf773ea6dab840
SteeRevo <69092521+SteeRevo@users.noreply.github.com>
2022-10-30 15:32:19 -0700

    added second portal camera

--- Jam2/Assets/Portal Resources/PortalTextureSetup.cs
+++ Jam2/Assets/Portal Resources/PortalTextureSetup.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PortalTextureSetup : MonoBehaviour
 {
     public Camera camera2;
+    public Camera camera1;
+
+    public Material cameraMat1;
 
     public Material cameraMat2;
 
@@ -15,5 +18,12 @@ public class PortalTextureSetup : MonoBehaviour
         }    
         camera2.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
         cameraMat2.mainTexture = camera2.targetTexture;
+
+        if (camera1.targetTexture != null)
+        {
+            camera1.targetTexture.Release();
+        }    
+        camera1.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMat1.mainTexture = camera1.targetTexture;
     }
 }
