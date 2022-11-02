19c7c0fe30e904a77ab8403e3f36ecab96043d0a
Ian Cowan <icowan@ucsc.edu>
2022-11-02 00:30:58 -0700

    added gravity and jumping

--- Jam2/Assets/Scripts/PlayerMovement.cs
+++ Jam2/Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,41 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        } else
+        {
+            velocity.y = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+        controller.Move(velocity * Time.deltaTime);
+
     }
 }
